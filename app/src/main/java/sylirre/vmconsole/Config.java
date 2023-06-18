88c6dfc7415b29e5004d03d3d93362cf0d4b8ecd
̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳ <3611208+goisneto@users.noreply.github.com>
2023-06-18 17:30:03 -0300

    Update Config.java

--- app/src/main/java/sylirre/vmconsole/Config.java
+++ app/src/main/java/sylirre/vmconsole/Config.java
@@ -60,7 +60,7 @@ public class Config {
     /**
      * Max RAM allocation in MiB which is considered to be safe.
      */
-    public static final int QEMU_MAX_SAFE_RAM = 2047;
+    public static final int QEMU_MAX_SAFE_RAM = 7629;
 
     /**
      * Minimal size of TCG buffer in MiB that would not cause too many
